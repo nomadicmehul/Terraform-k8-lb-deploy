dcf05499e0d5671327ef712495cbf056cfd89762
Mehul Patel <rowdymehul@users.noreply.github.com>
2020-04-28 02:58:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,3 +27,49 @@ resource "google_compute_subnetwork" "default" {
   private_ip_google_access = true
 }
 
+data "google_client_config" "current" {
+}
+
+data "google_container_engine_versions" "default" {
+  location = var.location
+}
+
+resource "google_container_cluster" "default" {
+  name               = var.network_name
+  location           = var.location
+  initial_node_count = 3
+  min_master_version = data.google_container_engine_versions.default.latest_master_version
+  network            = google_compute_subnetwork.default.name
+  subnetwork         = google_compute_subnetwork.default.name
+
+  // Use legacy ABAC until these issues are resolved:
+  //   https://github.com/mcuadros/terraform-provider-helm/issues/56
+  //   https://github.com/terraform-providers/terraform-provider-kubernetes/pull/73
+  enable_legacy_abac = true
+// Wait for the GCE LB controller to cleanup the resources.
+  // Wait for the GCE LB controller to cleanup the resources.
+  provisioner "local-exec" {
+    when    = destroy
+    command = "sleep 90"
+  }
+}
+
+output "network" {
+  value = google_compute_subnetwork.default.network
+}
+
+output "subnetwork_name" {
+  value = google_compute_subnetwork.default.name
+}
+
+output "cluster_name" {
+  value = google_container_cluster.default.name
+}
+
+output "cluster_region" {
+  value = var.region
+}
+
+output "cluster_location" {
+  value = google_container_cluster.default.location
+}
